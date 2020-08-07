7bd2c7a1a42bfc8b3084b4cd14b5caf22fff38f2
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-08-07 12:14:43 -0600

    added course

---
+++ src/components/library/libraryCourse.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class LibraryCourse extends Component {
+    render() {
+        return (
+            <div className="library-course">
+                <label className="library-course__title">Problem Solving</label>
+                {/* icon component */}
+                {/* arrow component */}
+                {/* action btn component */}
+
+                <div className="library-course__description">
+                    <label>Course Description</label>
+                    <p> Dummy text here </p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default LibraryCourse;
